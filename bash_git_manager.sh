a950d59817d8fa72777116b63a8d2337bb316821
guilherme simoes <guilherme.simoes@kabum.com.br>
2021-11-08 16:38:17 -0300

    refactor

--- bash_git_manager.sh
+++ bash_git_manager.sh
@@ -12,9 +12,12 @@ git_remote_url=$(git remote get-url origin);
 gh auth login --with-token <seu_token> 
 url_qa=$(gh pr create --title "$titulo_pr" --body "$body_da_pr" --base qa --head <seu_usuario>:$branch_name)
 
-echo $url_qa
+echo "Pull request pra qa => $url_qa"
 
 body_da_pr_master_e_stg="qa => $url_qa <br> $body_da_pr";
 
-gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base stg --head <seu_usuario>:$branch_name; 
-gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base master --head <seu_usuario>:$branch_name; 
+url_stg=$(gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base stg --head <seu_usuario>:$branch_name; )
+echo "Pull request pra stg => $url_stg"
+
+url_master=$(gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base master --head <seu_usuario>:$branch_name; )
+echo "Pull request pra master => $url_master"
