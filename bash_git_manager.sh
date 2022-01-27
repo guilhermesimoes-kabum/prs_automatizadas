8118d82f9b5d810b765c0a095b46ac8d31a0312d
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-01-27 11:09:39 -0300

    feat: a partir de agora ele roda os testes antes de fazer a PR, caso falharem, ele não deixa subir

--- bash_git_manager.sh
+++ bash_git_manager.sh
@@ -6,18 +6,28 @@ read titulo_pr
 echo "body da pr: "
 read body_da_pr 
 
+user_name=<seu_nome>
 branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
 git_remote_url=$(git remote get-url origin); 
 
 gh auth login --with-token <seu_token> 
-url_qa=$(gh pr create --title "$titulo_pr" --body "$body_da_pr" --base qa --head <seu_usuario>:$branch_name)
 
-echo "Pull request pra qa => $url_qa"
+#testes de nfe não podem falhar pra subir a pr
+resultado_testes=`prove -r GLOBAL/cgi-local/module/NFE2 && prove -r GLOBAL/cgi-local/module/NFEAPI && prove -r ./GLOBAL/cgi-local/module/AnalisesInterna/ && prove -r ./GLOBAL/cgi-local/module/ConciliacaoPIX/t && prove -r ./GLOBAL/cgi-local/module/Manager/Repository && prove -r ./GLOBAL/cgi-local/module/RegrasAutoCaptura/t/`
 
-body_da_pr_master_e_stg="qa => $url_qa <br> $body_da_pr";
-
-url_stg=$(gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base stg --head <seu_usuario>:$branch_name; )
-echo "Pull request pra stg => $url_stg"
-
-url_master=$(gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base master --head <seu_usuario>:$branch_name; )
-echo "Pull request pra master => $url_master"
+if [[ $resultado_testes =~ 'Result: FAIL' ]]; then
+	RED=`tput setaf 1`
+	echo "${RED}OS TESTES FALHARAM. Favor corrigir antes de subir"
+else
+	url_qa=$(gh pr create --title "$titulo_pr" --body "$body_da_pr" --base qa --head $user_name:$branch_name)
+	
+	echo "Pull request pra qa => $url_qa"
+	
+	body_da_pr_master_e_stg="qa => $url_qa <br> $body_da_pr";
+	
+	url_stg=$(gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base stg --head $user_name:$branch_name; )
+	echo "Pull request pra stg => $url_stg"
+	
+	url_master=$(gh pr create --title "$titulo_pr" --body "$body_da_pr_master_e_stg" --base master --head $user_name:$branch_name; )
+	echo "Pull request pra master => $url_master"
+fi
